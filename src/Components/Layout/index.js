863169e7db83b8e26f8de5dd1b4081aa34c7734e
Sovereign2 <saadamawi@yahoo.com>
2024-01-11 13:26:05 +0400

    s

---
+++ src/Components/Layout/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from '../Sidebar'
+
+function Layout() {
+  return (
+
+    <div className='side'>
+      <Sidebar/>
+    </div>
+  )
+}
+
+export default Layout
